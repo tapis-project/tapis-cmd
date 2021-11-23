7b6c23d271cb1e806508c6334ebfe257719fdd29
Richard Cardone <rcardone@tacc.utexas.edu>
2021-11-23 09:17:22 -0600

    SKGetAdmins

--- src/main/java/edu/utexas/tacc/tapis/cmd/driver/SKGetAdmins.java
+++ src/main/java/edu/utexas/tacc/tapis/cmd/driver/SKGetAdmins.java
@@ -2,6 +2,8 @@ package edu.utexas.tacc.tapis.cmd.driver;
 
 import java.util.Properties;
 
+import org.apache.commons.lang3.StringUtils;
+
 import edu.utexas.tacc.tapis.security.client.SKClient;
 
 /** This test class exercises SK's previewPathPrefix API to illustrate how it works
@@ -23,6 +25,9 @@ public class SKGetAdmins
         //----------------------- VALIDATE PARMS -----------------------//
         if(parms.jwtFilename == null)
             throw new Exception("jwtFilename is null and is required for appCreate, THROWING ERROR");
+        
+        if (StringUtils.isBlank(parms.tenant))
+            throw new Exception("-tenant must be provided.");    
         
         //----------------------- READ IN JWT PROFILE -----------------------//
         // Read base url and jwt from file.
