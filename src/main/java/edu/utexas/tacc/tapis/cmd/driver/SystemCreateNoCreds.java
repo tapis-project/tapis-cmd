faa1676874dbf912c5d455fc8c1cc756974387a2
Richard Cardone <rcardone@tacc.utexas.edu>
2022-04-23 19:44:36 -0500

    uh training34 definitions

---
+++ src/main/java/edu/utexas/tacc/tapis/cmd/driver/SystemCreateNoCreds.java
@@ -0,0 +1,53 @@
+package edu.utexas.tacc.tapis.cmd.driver;
+
+import java.util.Properties;
+
+import edu.utexas.tacc.tapis.shared.utils.TapisGsonUtils;
+import edu.utexas.tacc.tapis.systems.client.SystemsClient;
+import edu.utexas.tacc.tapis.systems.client.gen.model.ReqPostSystem;
+public class SystemCreateNoCreds
+{	
+    /** Creates a system
+     * 
+     * SystemCreate -jwt <jwt filename located in $HOME/Tapis-cmd/jwt> -req <name of json request file located in driver/requests>
+     * -pubKey <full path to public key> -privKey <full path to private key>
+     */
+    public static void main(String[] args) throws Exception
+    {
+    	//----------------------- INITIALIZE PARMS -----------------------//
+    	CMDUtilsParameters parms = null;
+    	try {parms = new CMDUtilsParameters(args);}
+        catch (Exception e) {
+          throw new Exception("Parms initialization for SystemCreate has failed with Exception: ",e);
+        }
+    	
+    	//----------------------- VALIDATE PARMS -----------------------//
+    	if(parms.reqFilename == null)
+    		throw new Exception("reqFilename is null and is required for SystemCreate operation, THROWING ERROR");
+    	
+    	if(parms.jwtFilename == null)
+    		throw new Exception("reqFilename is null and is required for SystemCreate operation, THROWING ERROR");
+    	
+        System.out.println("Processing " + parms.reqFilename + ".");
+        
+        //----------------------- READ JSON REQUEST INTO REQ OBJECT -----------------------//
+        // Convert json string into an app create request.
+        ReqPostSystem sysReq = TapisGsonUtils.getGson().fromJson(TestUtils.readRequestFile(parms.reqFilename), ReqPostSystem.class);
+        
+        //----------------------- READ IN JWT PROFILE -----------------------//
+        // Read base url and jwt from file.
+        Properties props = TestUtils.getTestProfile(parms.jwtFilename);
+        
+        //----------------------- CREATE CLIENT OBJECT -----------------------//
+        // Create the system.
+        var sysClient = new SystemsClient(props.getProperty("BASE_URL"), props.getProperty("USER_JWT"));
+  
+        //----------------------- ASSIGN OBO USER AND TENANT -----------------------//
+        if(parms.oboTenant != null)
+        	TestUtils.setOboHeaders(sysClient, parms.oboUser, parms.oboTenant);
+        
+        //----------------------- USE CLIENT OBJECT -----------------------//
+        sysClient.createSystem(sysReq);
+        System.out.println("Finished processing " + parms.reqFilename);
+    }
+}
